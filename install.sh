9654ff21864daf26be49b3f362655b15ede6a9af
Donat Müller <52152977+donat-m@users.noreply.github.com>
2019-12-08 04:43:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 # Download Unity3D installer into the container
 #  The below link will need to change depending on the version, this one is for 5.5.1
 #  Refer to https://unity3d.com/get-unity/download/archive and find the link pointed to by Mac "Unity Editor"
-echo 'Downloading Unity 5.5.1 pkg:'
-curl --retry 5 -o Unity.pkg http://netstorage.unity3d.com/unity/88d00a7498cd/MacEditorInstaller/Unity-5.5.1f1.pkg
+echo 'Downloading Unity 2018.1.1f1 pkg:'
+curl --retry 5 -o Unity.pkg https://download.unity3d.com/download_unity/b8cbb5de9840/MacEditorInstaller/Unity.pkg
 if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
 # In Unity 5 they split up build platform support into modules which are installed separately
 # By default, only Mac OSX support is included in the original editor package; Windows, Linux, iOS, Android, and others are separate
 # In this example we download Windows support. Refer to http://unity.grimdork.net/ to see what form the URLs should take
-echo 'Downloading Unity 5.5.1 Windows Build Support pkg:'
-curl --retry 5 -o Unity_win.pkg http://netstorage.unity3d.com/unity/88d00a7498cd/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-5.5.1f1.pkg
+echo 'Downloading Unity 2018.1.1f1 Windows Build Support pkg:'
+curl --retry 5 -o Unity_win.pkg https://download.unity3d.com/download_unity/b8cbb5de9840/true
 if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
 # Run installer(s)
